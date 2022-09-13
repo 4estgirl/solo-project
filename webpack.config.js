4c2a68455808fa35df10a016be04759e51ab2c1b
Abigail Gerig <abigailgerig@Abigails-MacBook-Pro.local>
2022-09-12 20:52:32 -0500

    Created initial files for solo-project

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname,'client/index.js'),
+
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
+        filename: 'bundle.js',
+    },
+
+    mode: process.env.NODE_ENV,
+    
+    devServer: {
+        static: {
+            directory: path.join(__dirname, './build'),
+        },
+        // proxy: {
+        //     '/api/leaders': `http://localhost:3000/`,
+        // }
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', `@babel/preset-react`]
+                    }
+                }
+            },
+            {
+                test: /\.s?css/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            }
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: '/index.html',
+      })],
+};
